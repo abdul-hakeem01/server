86496d0c552a383c91ff14e36cd698dd6270dcda
abdul-hakeem01 <166890202+abdul-hakeem01@users.noreply.github.com>
2024-10-04 09:58:03 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
